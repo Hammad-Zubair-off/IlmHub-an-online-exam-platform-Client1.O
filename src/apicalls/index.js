cc23531c885a33dd551de7411759656448da519c
Hammad-Zubair-Off <hammadzubairofficial@gmail.com>
2025-05-18 11:23:21 +0500

    Base url ;

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 import { API_URL } from '../config';
 
+console.log('API Base URL:', API_URL);
+
 const axiosInstance = axios.create({
     baseURL: API_URL,
     headers: {
          Authorization : `Bearer ${localStorage.getItem('token')}`
     }
+});
+
+// Add request interceptor to log all requests
+axiosInstance.interceptors.request.use(request => {
+    console.log('Starting Request:', request.url);
+    return request;
 });
 
 export default axiosInstance;
